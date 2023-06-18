3573d106d21fc138ede042815651cc527bbf62df
winebarrel <sugawara@winebarrel.jp>
2023-06-18 23:21:16 +0900

    Update next command: Add --format option

--- cmd/subcmd/next_test.go
+++ cmd/subcmd/next_test.go
@@ -59,3 +59,28 @@ func TestNextCmd_5(_t *testing.T) {
 2023-10-09	スポーツの日
 `, out.String())
 }
+
+func TestNextCmd_Format(_t *testing.T) {
+	assert := assert.New(_t)
+	out := &strings.Builder{}
+
+	cmd := &subcmd.Next{
+		N:      3,
+		Format: "%Y/%m/%d(%a)",
+	}
+
+	err := cmd.Run(&subcmd.Binds{
+		Client: Client,
+		Out:    out,
+		Now:    func() time.Time { return dateparse.MustParse("2023-07-17") },
+	})
+
+	if !assert.NoError(err) {
+		return
+	}
+
+	assert.Equal(`2023-07-17(Mon)	海の日
+2023-08-11(Fri)	山の日
+2023-09-18(Mon)	敬老の日
+`, out.String())
+}
